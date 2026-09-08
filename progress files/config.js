bb199f6dc6268cc6d96b86170fc02a128c8072bd
Roky777 <paulroky78@gmail.com>
2026-09-08 16:56:20 +0530

    Use unique Shape progress storage key

--- progress files/config.js
+++ progress files/config.js
@@ -23,7 +23,7 @@ const CONFIG = {
 
   // Storage Configuration
   storage: {
-    storageKey: 'brainMatch-periodic_progress',
+    storageKey: 'brainMatch-shape_progress',
     useAsyncStorage: false, // Set to true for React Native
   },
 
